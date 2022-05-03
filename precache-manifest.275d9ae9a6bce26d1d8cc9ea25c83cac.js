b422822febbbfbce4d3681b9df15d269de81cc5c
hetalsagpariya <hetalsagpariya@gmail.com>
2022-05-03 15:33:09 +0530

    Updates

---
+++ precache-manifest.275d9ae9a6bce26d1d8cc9ea25c83cac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6c0d120cf34938b20265ca79c44b83",
+    "url": "/react-NewApp/index.html"
+  },
+  {
+    "revision": "02aa78597b13229bde6f",
+    "url": "/react-NewApp/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "387aa290aebf81cb6c28",
+    "url": "/react-NewApp/static/js/2.ec6dda78.chunk.js"
+  },
+  {
+    "revision": "5d06bb858cc186fdb179",
+    "url": "/react-NewApp/static/js/3.b019360d.chunk.js"
+  },
+  {
+    "revision": "02aa78597b13229bde6f",
+    "url": "/react-NewApp/static/js/main.7ab46928.chunk.js"
+  },
+  {
+    "revision": "ac89d9ffe8d00b6ba4ab",
+    "url": "/react-NewApp/static/js/runtime~main.991872a2.js"
+  },
+  {
+    "revision": "747d0ffa1afac19d953a1237c75399ec",
+    "url": "/react-NewApp/static/media/loading1.747d0ffa.gif"
+  }
+]);
